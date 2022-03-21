73fc6328d652354466db31ac0bcfb40b5c98c0b5
mmmmmm9 <75459104+mmmmmm9@users.noreply.github.com>
2022-03-21 00:34:43 -0400

    Delete santa directory

--- santa/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.gif",
-    preGameScreenImageURLs:"./img/pre-screen.jpg",
-    postGameScreenImageURLs:"./img/post-screen.jpg", 
-    playUntilScore:(10),
-    actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, 
-    actionDuration:600, jumpHeight: 400,npcSpeed:300,typesOfNPC:(3)});
-});
